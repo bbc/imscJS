ba4158b977432479b70ab7a51ead1e3663067cf1
Robert Bryer <robert.bryer@bbc.co.uk>
2020-08-04 18:11:52 +0100

    Make default font family give proportionalSansSerif, remove whitespace from fonts

--- src/main/js/html.js
+++ src/main/js/html.js
@@ -1380,6 +1380,7 @@
                     /* per IMSC1 */
 
                     for (var i in attr) {
+                        attr[i] = attr[i].trim();
 
                         if (attr[i] === "monospaceSerif") {
 
@@ -1388,7 +1389,7 @@
                             rslt.push("Courier");
                             rslt.push("monospace");
 
-                        } else if (attr[i] === "proportionalSansSerif") {
+                        } else if (attr[i] === "proportionalSansSerif" || attr[i] === "default") {
 
                             rslt.push("Arial");
                             rslt.push("Helvetica");
