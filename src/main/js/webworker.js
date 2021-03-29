a80bb03ca71e75f9ed5faa90b4d67df08b28a8d9
Robert Bryer <robert.bryer@bbc.co.uk>
2021-03-29 18:12:09 +0100

    Catch parser errors and put into thrown/fatal error catcher, post errors with severity

--- src/main/js/webworker.js
+++ src/main/js/webworker.js
@@ -3,27 +3,43 @@ var generateISD = require('./isd').generateISD;
 var fromXML = require('./doc').fromXML;
 
 
+var errorHandler = {
+    info: sendError.bind(null, "info"),
+    warn: sendError.bind(null, "warn"),
+    error: sendError.bind(null, "error"),
+    // There is a fatal error handler – but it still throws exceptions.
+    // We still want to try-catch other exceptions – so don't listen to "fatal",
+    // Catch both deliberate fatal exceptions and unexpected ones and push them both to thrownFatal
+    thrownFatal: sendError.bind(null, "thrownFatal")
+}
+
+function sendError(severity, message) {
+    postMessage({
+        error: {
+            severity: severity,
+            message: message
+        }
+    });
+
+    return false;
+}
+
 onmessage = function(event) {
     const xmlStr = event.data.xmlStr;
     if (typeof xmlStr === "string") {
-        const tt = fromXML(xmlStr, errorHandler);
-        // We can't serialise functions, call the function now just in case this function
-        // no longer returns.
-        tt.mediaTimeEvents = tt.getMediaTimeEvents();
+        try {
+            const tt = fromXML(xmlStr, errorHandler);
+            // We can't serialise functions, call the function now just in case this function
+            // no longer returns.
+            tt.mediaTimeEvents = tt.getMediaTimeEvents();
 
-        postMessage({
-            result: tt
-        });
+            postMessage({
+                result: tt
+            });
+        } catch (e) {
+            errorHandler.thrownFatal(e);
+        }
     } else {
-        postMessage({
-            error: "Invalid argument" + xmlStr
-        });
+        errorHandler.thrownFatal("Invalid argument" + xmlStr);
     }
 };
-
-function errorHandler(err) {
-    console.error(err);
-    postMessage({
-        error: err
-    });
-}
