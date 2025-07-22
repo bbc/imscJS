923f3446f062bc616e98b7f46fb15ad5f50d33a0
Jim <5435022+JibberJim@users.noreply.github.com>
2025-07-22 13:44:29 +0100

    SMPWEB-3067 add attribute if there should be a line break

--- src/main/js/html.js
+++ src/main/js/html.js
@@ -681,6 +681,11 @@ var browserIsFirefox = /firefox/i.test(navigator.userAgent);
 
                 }
             }
+            // add the breakafter attribute to any lines until the
+            // last to workaround chrome calculation being wrong
+            if (l<(lineList.length-1) && lineList[l].elements[0] && lineList[l].elements[0].node) {
+              lineList[l].elements[0].node.setAttribute("breakafter","breakafter");
+            }
         }
 
         for (var bi = 0; bi < clearTheseBackgrounds.length; bi++) {
