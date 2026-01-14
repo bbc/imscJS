ca12a9ecc34b4baf4ecb7b71b00e11f9cbc799cf
James Reed <jamesreedcode@users.noreply.github.com>
2026-01-14 11:52:17 +0000

    SMPWEB-3117 Off Maven

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,9 +1,14 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 import gulp from "gulp";
 import {rollup} from 'rollup';
 import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs";
 import nodePolyfills from 'rollup-plugin-node-polyfills';
+const gzip = require('gulp-gzip');
+const pjson = require('./package.json');
 
 //no unit tests or jshint or anything, just piggyback on the grunt ones and assume it'll be run together.
 
@@ -32,6 +37,14 @@ function bundle(debug) {
         });
 };
 
+function deploy() {
+    return gulp.src('dist/**').pipe(gzip({
+        append: false,
+        gzipOptions: { level: 9 }
+    })).pipe(gulp.dest('deploy/imsc/' + pjson.version + '/'));
+}
 
-export default gulp.parallel(bundle.bind(null, true), bundle.bind(null, false));
+const release = gulp.parallel(bundle.bind(null, true), bundle.bind(null, false));
 
+
+export {release, deploy};
