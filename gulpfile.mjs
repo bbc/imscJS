89301ee17ba804e1a181239be6a6739f7ddace3d
James Reed <jamesreedcode@users.noreply.github.com>
2026-02-03 16:23:51 +0000

    SMPWEB-3117 Off Maven

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -7,10 +7,19 @@ import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs";
 import nodePolyfills from 'rollup-plugin-node-polyfills';
+import yargs from 'yargs';
+import pjson from './package.json' with { type: "json" };
+import {rimraf} from 'rimraf';
+
 const gzip = require('gulp-gzip');
-const pjson = require('./package.json');
 
 //no unit tests or jshint or anything, just piggyback on the grunt ones and assume it'll be run together.
+
+const yargv = yargs(process.argv.slice(2)).parse();
+
+function cleanDeploy() {
+    return rimraf("deploy");
+}
 
 function bundle(debug) {
     const inConfig = {
@@ -37,14 +46,19 @@ function bundle(debug) {
         });
 };
 
-function deploy() {
+function moveFiles() {
+
+    const version = (yargv.env === 'int') ? 'int' : pjson.version;
+
     return gulp.src('dist/**').pipe(gzip({
         append: false,
         gzipOptions: { level: 9 }
-    })).pipe(gulp.dest('deploy/imsc/' + pjson.version + '/'));
+    })).pipe(gulp.dest('deploy/imsc/' + version + '/'));
+
 }
 
 const release = gulp.parallel(bundle.bind(null, true), bundle.bind(null, false));
 
+const deploy = gulp.series(cleanDeploy, moveFiles);
 
 export {release, deploy};
